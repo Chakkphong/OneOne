5d0066c003271c1c20329c2f81effe164b0a1fa6
Administrator <Administrator@10.240.19.46>
2014-05-30 15:52:16 +0700

    OneOne

---
+++ src/oneone/OneOne.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package oneone;
+
+/**
+ *
+ * @author Administrator
+ */
+// /* programs need
+//      comments */
+public class OneOne {
+    private static String aStirng;
+    
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        aStirng = "Well!";
+        String OneOne = "Now!";
+        System.out.println("Isn't this an interesting program");
+        System.out.println("Answer yes or no!");
+    }
+    
+}
